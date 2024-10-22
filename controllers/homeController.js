f5a2322c00971c25b2329c7646c08724d7b8bdf0
isaaconek <onekdev5@gmail.com>
2024-10-22 17:16:07 +0100

    Recuperation et affichage du principale end-point de mon api

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,9 @@
 const HomeController = (req, res) => {
     console.log('Nouvelle requette');
     res.status = 200
-    res.send ('Bienvenue')
+    // Je recupere le end point de mon api
+    const endPoint1 = `${req.protocol}://${req.get('host')}`
+    res.send (`<h1>Bienvenu dans mon api: <a href='${endPoint1}' >${endPoint1}<a/> </h1>`)
 }
  module.exports = {
     HomeController
