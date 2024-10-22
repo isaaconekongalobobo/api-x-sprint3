7a3117b36d234213996e4708c3d0665b39611a7a
isaaconek <onekdev5@gmail.com>
2024-10-22 17:05:36 +0100

    Envoie de l'url de l'image dans le corps de la reponse

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express  = require ('express')
 const app = express ()
 
-// Middleware pour parser les données du formulaire
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 const cors = require ('cors')
 const { HomeController } = require('./controllers/homeController')
 const multer = require ('multer')
 const path = require ('path')
 
-
+// Middleware pour parser les données du formulaire
+app.use(express.urlencoded({ extended: true }));
+// Pour le Json
+app.use(express.json());
+// Pour autoriser les requettes de tout les autres domaines
+app.use (cors())
+// Pour rendre accessible les fichiers statiques de mon projet
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Configuration de Multer
 const storage = multer.diskStorage({
@@ -30,8 +33,14 @@ const port = process.env.PORT || 5000
 
 app.get ('/', (req, res) => HomeController(req, res))
 app.post ('/uploads/image', upload.single ('image'), (req, res) => {
-    console.log("Requette pour l'upload");
-    console.log(req.file);
+    if (req.file) {
+        // Je construit l'url de l'image, qui la rendra accessible de partout
+        const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`
+        // Je renvoie l'url genere a mon client react
+        res.status (200).json ({imageUrl})
+    } else {
+        res.status (400).send ('Aucune image recus')
+    }
 })
 
 app.listen (port, () => {
